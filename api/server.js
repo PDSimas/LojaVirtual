3be6f9e4d3f88583517733f30441766f785cda5d
Patrick Diniz <simas.patrickdiniz@gmail.com>
2024-05-22 12:37:31 -0300

    confg do cors

--- api/server.js
+++ api/server.js
@@ -1,4 +1,13 @@
+const express = require('express');
+const cors = require('cors');
 const jsonServer = require('json-server');
+
+const app = express();
+
+// Configurar o CORS
+app.use(cors());
+
+// Configurações do JSON Server
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
