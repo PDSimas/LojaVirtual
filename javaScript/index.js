221124ecb9c6199a7ed92704ecb86972674acf95
Patrick Diniz <simas.patrickdiniz@gmail.com>
2024-05-22 11:51:44 -0300

    cors

---
+++ javaScript/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Permitir apenas uma origem específica
+const corsOptions = {
+  origin: 'https://loja-virtual-plum.vercel.app/', // URL do seu frontend
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+// Exemplo de rota
+app.get('/produtos', (req, res) => {
+    res.json({ message: "Olá, produtos!" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
